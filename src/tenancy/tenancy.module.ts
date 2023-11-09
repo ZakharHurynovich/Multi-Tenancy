cd85d4492b116c33b10c7f611aafeec33408d8eb
Zakhri <zakhar.hurinovich@digifi.io>
2023-11-09 11:39:35 +0100

    Update changed tenancy provider -> tenancy service

--- src/tenancy/tenancy.module.ts
+++ src/tenancy/tenancy.module.ts
@@ -1,13 +1,26 @@
 import { Global, Module } from '@nestjs/common';
 import { DatabaseTypes } from 'src/app.types';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import databaseProvider from './tenancy.provider';
+import { TenancyName } from './tenancy.enum';
+import TenancyService from './tenancy.service';
+import { REQUEST } from '@nestjs/core';
 
 @Global()
 @Module({
   imports: [TypeOrmModule],
   controllers: [],
-  providers: [databaseProvider()],
+  providers: [
+    {
+      provide: DatabaseTypes.DataSource,
+      useFactory: async (request: Request) => {
+        const header = request.headers['x-tenant-name'] as TenancyName;
+
+        return new TenancyService().connection(header);
+      },
+      inject: [REQUEST],
+    },
+    TenancyService,
+  ],
   exports: [DatabaseTypes.DataSource],
 })
 export class TenancyModule {}
